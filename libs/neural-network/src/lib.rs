493fa40b2a0f050f09126b5cdb8d839910a7bdff
TotallyYourGrandpa <bbbaj2404@gmail.com>
2024-10-29 19:46:00 -0600

    Add main scaffolding for neural network

---
+++ libs/neural-network/src/lib.rs
@@ -0,0 +1,48 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+
+// Everything above was default example code
+
+#[derive(Debug)]
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+impl Network {
+    pub fn propagate(&self, mut inputs: Vec<f32>) -> Vec<f32> {
+        for layer in &self.layers {
+            inputs = layer.propagate(inputs);
+        }
+
+        return inputs
+    }
+}
+
+#[derive(Debug)]
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        todo!()
+    }
+}
+
+#[derive(Debug)]
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
