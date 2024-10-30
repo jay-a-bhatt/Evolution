84c29abe7bdb3f841bee2e56b6684f2e71977e80
TotallyYourGrandpa <bbbaj2404@gmail.com>
2024-10-29 20:16:29 -0600

    Add Neuron and Layer methods

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -15,6 +15,8 @@ mod tests {
 
 // Everything above was default example code
 
+// TODO - Coding: new() section in https://pwy.io/posts/learning-to-fly-pt2/
+
 #[derive(Debug)]
 pub struct Network {
     layers: Vec<Layer>,
@@ -22,7 +24,7 @@ pub struct Network {
 
 impl Network {
     pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        self.layers
+        return self.layers
             .iter()
             .fold(inputs, |inputs, layer| layer.propagate(inputs))
         // Above is the same as
@@ -39,7 +41,17 @@ struct Layer {
 
 impl Layer {
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        todo!()
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+        // Above is the same as
+        // let mut outputs = Vec::new();
+        // for neuron in &self.neurons {
+        //     let output = neuron.propagate(&inputs);
+        //     outputs.push(output);
+        // }
+        // return outputs
     }
 }
 
@@ -47,4 +59,30 @@ impl Layer {
 struct Neuron {
     bias: f32,
     weights: Vec<f32>,
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        assert_eq!(inputs.len(), self.weights.len());
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+        return (self.bias + output).max(0.0)
+        // Above let and return statement same as 
+        // let mut output = 0.0;
+        // 
+        // for i in 0..inputs.len() {
+        //     output += inputs[i] * self.weights[i];
+        // }
+        //     
+        // output += self.bias;
+        // 
+        // if output > 0.0 {
+        //     output
+        // } else {
+        //     0.0
+        // }
+    }
 }
